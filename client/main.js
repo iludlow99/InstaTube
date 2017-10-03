490eb4d35eeefed3a5c9ea57e14310b4a5b4a018
Isaac Ludlow <isaac@theludlows.org>
2017-10-03 19:17:49 -0400

    Broken code

---
+++ client/main.js
@@ -0,0 +1,11 @@
+// var video = {
+//     vid_1:'https://www.youtube.com/embed/xrq_xtm_qqQ'
+// };
+
+// $('#videos')(function videoReturn(video) {
+
+//              return video
+//              });
+
+// videoReturn(vid-1)
+
