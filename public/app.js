50d68d830f9217589cee557e02be6f24e3ea61f3
Isaac Ludlow <isaac@theludlows.org>
2017-10-04 21:45:11 -0400

    fixed tables

---
+++ public/app.js
@@ -0,0 +1,15 @@
+//(function() {
+
+	// Initialize Firebase
+  	var config = {
+    apiKey: "AIzaSyCy1x43XE-TIh_1AiQnybgD6ScB-s2sWPo",
+    authDomain: "patientstories-84822.firebaseapp.com",
+    databaseURL: "https://patientstories-84822.firebaseio.com",
+    projectId: "patientstories-84822",
+    storageBucket: "",
+    messagingSenderId: "884104850202"
+  };
+  firebase.initializeApp(config);
+
+//})();
+
